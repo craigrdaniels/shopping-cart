372a2c2fe7844569db7dde6ef174730a73cd1c1b
Craig Daniels <craigrdaniels@gmail.com>
2022-11-05 20:42:15 +1000

    rename router file to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Shop from "./pages/Shop";
+import Item from "./pages/Item";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/shop/item/*" element={<Item />} />
+        <Route exact path="/shop" element={<Shop />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
