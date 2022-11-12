de169d988047aac6c1f5171e95f40fe47688ac15
Craig Daniels <craigrdaniels@gmail.com>
2022-11-12 15:38:14 +1000

    add home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import PopularItems from "./PopularItems";
 
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <div className="relative bg-indigo-800 group">
+        <div className="absolute inset-0 overflow-hidden">
+          <img
+            className="h-full w-full object-cover object-top group-hover:scale-105 transition duration-700 ease-in-out"
+            src="/images/hannah-morgan-ycVFts5Ma4s-unsplash.jpg"
+            alt=""
+          />
+          <div
+            className="absolute inset-0 bg-gradient-to-r from-gray-200 to-gray-400 mix-blend-multiply"
+            aria-hidden="true"
+          />
+        </div>
+        <div className="relative mx-auto max-w-8xl py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
+          <h1 className="text-right text-2xl tracking-tight [text-shadow:_1px_1px_0_rgb(0_0_0_/_40%)] text-white sm:text-5xl lg:text-6xl">
+            Fashion direct to your doorstep
+          </h1>
+          <p className="text-right mt-6 text-xl [text-shadow:_1px_1px_0_rgb(0_0_0_/_40%)] text-white">
+            Become a Fashionista without leaving your home
+          </p>
+          <p className="text-right mt-6 text-xl">
+            <Link
+              to="/shop"
+              className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Shop Now
+            </Link>
+          </p>
+        </div>
+      </div>
+      <PopularItems />
+    </>
+  );
 };
 
 export default Home;
---
+++ src/components/pages/PopularItems.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const PopularItems = () => {
+  const [loading, setLoading] = useState(true);
+  const [popularItems, setPopularItems] = useState([]);
+
+  useEffect(() => {
+    getPopularItems();
+  }, []);
+
+  const getPopularItems = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("https://fakestoreapi.com/products?limit=4");
+      const products = await response.json();
+      setPopularItems(products);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error(`Unable to fetch products from URL`);
+    }
+  };
+
+  return (
+    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+        Popular Items
+      </h2>
+
+      <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        {popularItems?.map((product) => (
+          <Link
+            key={product.id}
+            to={"/shop/item/".concat(product.id)}
+            className="group"
+          >
+            <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-white hover:opacity-75 lg:aspect-none lg:h-80">
+              <img
+                src={product.image}
+                alt={product.title}
+                className="h-full w-full object-contain object-center lg:h-full lg:w-full hover:opacity-75 transition"
+              />
+            </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  {product.title.length > 36
+                    ? product.title.substring(0, 24) + "..."
+                    : product.title}
+                </h3>
+              </div>
+              <p className="text-sm font-medium text-gray-900">
+                {product.price.toLocaleString("en-AU", {
+                  style: "currency",
+                  currency: "AUD",
+                })}
+              </p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PopularItems;
